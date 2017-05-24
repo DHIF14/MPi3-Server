f9f3c062eff63c7ed6461377a27439f7fd64792e
benedikt <benedikt@benedikt-Lenovo-Yoga-500-14ISK>
2017-05-24 12:01:19 +0200

    Made Object structure for Song (Name and Path)

--- src/player/Song.java
+++ src/player/Song.java
@@ -3,4 +3,20 @@ package player;
 /**
  * Created by Michael Krickl and Benedikt Breid in 2017.
  */
-public class Song {}
+public class Song {
+    String name;
+    String path;
+
+    public Song(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
