b906752555c3e78f0d5c0003ca5f55b536dba934
benedikt <benedikt@benedikt-Lenovo-Yoga-500-14ISK>
2017-05-24 13:12:36 +0200

    Made Object structure for Song (Name and Path) Added custom test package under player with a PlaybackTester Implemented playSong in Playback

--- src/player/Playback.java
+++ src/player/Playback.java
@@ -1,6 +1,37 @@
 package player;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
 /**
  * Created by Michael Krickl and Benedikt Breid in 2017.
  */
-public class Playback {}
+public class Playback {
+    private final Logger logger=Logger.getLogger("Playback");
+    private static final Playback instance=new Playback();
+    private final AudioPlayer player=AudioPlayer.player;
+    
+    private Playback(){};
+    
+    public static Playback getInstance(){
+        return instance;
+    }
+    
+    public void playSong(Song song) 
+            throws FileNotFoundException{
+        try {
+            InputStream in = new FileInputStream(song.path);
+            AudioStream audioStream = new AudioStream(in);
+            AudioPlayer.player.start(audioStream);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Exception thrown when creating a new AudioStream: {0}", e.getMessage());
+        }
+        
+    }
+}
--- src/player/Player.java
+++ src/player/Player.java
@@ -3,4 +3,6 @@ package player;
 /**
  * Created by Michael Krickl and Benedikt Breid in 2017.
  */
-public class Player {}
+public class Player {
+    
+}
