ec12e01b583ccec39e7c7638c4f80c0e781e8331
Michael Krickl <ch.michael.krickl@gmail.com>
2017-05-24 09:48:48 +0200

    added simple Launcher

---
+++ src/start/Launcher.java
@@ -0,0 +1,13 @@
+package start;
+
+import net.Server;
+
+/**
+ * Created by Michael Krickl in 2017.
+ */
+public class Launcher {
+  
+  public static void main(String[] args) {
+    new Server();
+  }
+}
