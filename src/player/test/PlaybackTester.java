c04e1500d96f72dd313d1bef712d4af70c78825e
Benetrationer <breid.benedikt@gmail.com>
2017-05-29 16:09:11 +0200

    Implemented relative Paths in PlaybackTester.

--- src/player/test/PlaybackTester.java
+++ src/player/test/PlaybackTester.java
@@ -17,7 +17,6 @@ import player.Song;
 public class PlaybackTester {
     public static void main(String[] args)
             throws FileNotFoundException{
-        System.out.println(System.getProperty("user.dir"));
         Playback playback=Playback.getInstance();
         playback.playSong(new Song("Test","Songs/a2002011001-e02.wav"));
     }
