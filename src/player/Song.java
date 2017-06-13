246233d7e4561d5ec07bdc19e0af3818db459d0a
Michael Krickl <ch.michael.krickl@gmail.com>
2017-06-13 03:10:56 +0200

    fixed benes BS

--- src/player/Song.java
+++ src/player/Song.java
@@ -1,27 +1,77 @@
 package player;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
 /**
  * Created by Michael Krickl and Benedikt Breid in 2017. Gepfuscht by feba6481
  */
 public class Song {
-    private final String name;
-    private String path;
-
-    public Song(String name, String path) {
-        this.name = name;
-        this.path = path;
-    }
+  private final String name;
+  private Path path;
+  
+  private static final Path SONGS_ROOT = Paths.get("/home/mpi3/songs");
+  private static List<Song> songs = new ArrayList<>();
+  
+  static {
     
-    public Song(String path){
-        this.path=path;
-        this.name=path.split("/")[path.split("/").length-1];
+    try {
+      songs = new ArrayList<>(
+          Files.walk(SONGS_ROOT)
+               .filter(p -> Files.isRegularFile(p))
+               .map(Song::new)
+               .distinct()
+               .collect(Collectors.toList()));
+    } catch (Exception e) {
+      Logger.getAnonymousLogger().severe("Exception when collecting songs: " + e.getMessage());
+      throw new RuntimeException("Exception when collecting songs", e);
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPath() {
-        return path;
-    }
+  }
+  
+  public static Song getSongByName(String name) {
+    return songs.stream()
+                .filter(s -> s.getName().equalsIgnoreCase(name))
+                .findFirst().orElse(null);
+  }
+  
+  public static Song getSongByPath(Path path) {
+    return songs.stream()
+                .filter(s -> s.getPath().equals(path))
+                .findFirst().orElse(null);
+  }
+  
+  // deprecated?
+  public Song(String name, String path) {
+    this.name = name;
+    this.path = Paths.get(toString());
+  }
+  
+  private Song(Path path) {
+    this.path = path;
+    this.name = path.getFileName().toString();
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public Path getPath() {
+    return path;
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+    
+    if(!(o instanceof Song)) return false;
+    
+    Song other = (Song) o;
+    if(!other.getName().equalsIgnoreCase(this.getName())) return false;
+    
+    return true;
+  }
 }
